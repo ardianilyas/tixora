87fef3164b87529da6ab59b748a7a7da20837391
ardianilyas <ardianilyas@gmail.com>
2026-08-11 14:49:29 +0700

    fix: prisma import and add table on clear db

--- tests/helpers/clear-db.ts
+++ tests/helpers/clear-db.ts
@@ -1,7 +1,7 @@
-import { prisma } from "../../src/db";
+import { prisma } from "../../src/shared/lib/prisma";
 
 export async function clearDb() {
   await prisma.$executeRawUnsafe(`
-    TRUNCATE TABLE "user", "session", "account", "verification" RESTART IDENTITY CASCADE;
+    TRUNCATE TABLE "user", "session", "account", "verification", "categories", "tickets" RESTART IDENTITY CASCADE;
   `);
 }
